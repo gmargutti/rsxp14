e69deff96717a0807f995335d89002d2b7f3f265
Gustavo <gustavo.margutti@rojemac.com.br>
2019-11-26 17:42:13 -0300

    Formatted Jobs

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,11 +3,13 @@ import React, { Fragment } from 'react';
 import Chatbot from '../../components/chatbot';
 
 import { Header, Mission, Cases } from './styles';
+import img from '../../../public/assets/1.png'
 
 export default function Main() {
   return (
     <Fragment>
       <Header>
+        <img src={img} />
         <h1>Por que ser um progamador?</h1>
         <Chatbot />
       </Header>
