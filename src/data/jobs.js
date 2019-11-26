9c98debb1f300b55db0672100d4bc621c3927dcf
Gustavo <gustavo.margutti@rojemac.com.br>
2019-11-26 18:09:21 -0300

    Changed jobs property from descr to description

--- src/data/jobs.js
+++ src/data/jobs.js
@@ -1,7 +1,7 @@
 export default [
   {
       img: 'https://jobs.solides.com/assets/img/logos_empresas/logo__teknisa.png',
-      descr: 'Irá compor a equipe de desenvolvimento atuando como full stack em um aplicativo de entretenimento! É imprescindível conhecimento em HTML5, CSS3, JavaScript e PHP. Buscamos uma pessoa que seja centrada, tenha o foco em resultados e engajado em aprender novos conhecimentos.',
+      description: 'Irá compor a equipe de desenvolvimento atuando como full stack em um aplicativo de entretenimento! É imprescindível conhecimento em HTML5, CSS3, JavaScript e PHP. Buscamos uma pessoa que seja centrada, tenha o foco em resultados e engajado em aprender novos conhecimentos.',
       cargo: 'Desenvolvedor Fullstack Júnior na Teknisa',
       salarioInicial: 'R$2000,00',
       salarioFinal: 'R$2500,00',
@@ -10,7 +10,7 @@ export default [
     },
     {
         img: 'https://api.vulpi.com.br/media/alves_O23k5RY.png',
-        descr: 'O core business da Apta é automatização industrial! Diante disso, o(a) profissional irá atuar na linha de frente do desenvolvimento front end dos produtos e projetos de cases elaboradores por clientes. ',
+        description: 'O core business da Apta é automatização industrial! Diante disso, o(a) profissional irá atuar na linha de frente do desenvolvimento front end dos produtos e projetos de cases elaboradores por clientes. ',
         cargo: 'Desenvolvedor Frontend Júnior na APTA Sistemas de Automação',
         salarioInicial: 'R$2800,00',
         salarioFinal: 'R$3200,00',
@@ -19,7 +19,7 @@ export default [
   },
   {
       img: 'https://livre.com.br/wp-content/uploads/2018/07/logo-cabecalho-livre.png',
-      descr: 'Irá compor a equipe de desenvolvimento atuando com foco no front end e é importante ter uma noção do back end. ',
+      description: 'Irá compor a equipe de desenvolvimento atuando com foco no front end e é importante ter uma noção do back end. ',
       cargo: 'Desenvolvedor Fullstack Júnior na Livre',
       salarioInicial: 'R$4500,00',
       salarioFinal: 'R$5000,00',
@@ -28,7 +28,7 @@ export default [
   },
   {
       img: 'https://www.rps.com.br/assets/images/logorps-66x87.png',
-      descr: 'Realizar alterações nos dados quando necessário. Analisar as demandas reportadas pelos clientes, propondo soluções existentes ou criando novas funcionalidades para o sistema ERP.',
+      description: 'Realizar alterações nos dados quando necessário. Analisar as demandas reportadas pelos clientes, propondo soluções existentes ou criando novas funcionalidades para o sistema ERP.',
       cargo: 'Analista de Sistemas Júnior na RPS',
       salarioInicial: 'R$4500,00',
       salarioFinal: 'R$5000,00',
@@ -37,7 +37,7 @@ export default [
     },
     {
       img: 'https://upload.wikimedia.org/wikipedia/commons/b/bf/Logo_drconsulta.png',
-      descr: 'Para nós, pacientes e profissionais da saúde estão em primeiro lugar e, para isso, precisamos ser ótimos CUIDADORES! Além disso, temos garra, senso de urgência e outros valores que nos levam a ser obstinados por resultados, afinal, estamos em constante crescimento e até 2023 queremos proporcionar acesso à saúde para 10 milhões de pacientes únicos!',
+      description: 'Para nós, pacientes e profissionais da saúde estão em primeiro lugar e, para isso, precisamos ser ótimos CUIDADORES! Além disso, temos garra, senso de urgência e outros valores que nos levam a ser obstinados por resultados, afinal, estamos em constante crescimento e até 2023 queremos proporcionar acesso à saúde para 10 milhões de pacientes únicos!',
       cargo: 'Especialista DevOps na Dr. Consulta',
       salarioInicial: 'R$12704,00',
       salarioFinal: 'R$12704,01',
