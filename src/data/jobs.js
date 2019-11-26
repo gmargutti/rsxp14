b59ce00386d990c741ec795a873b680957ef3613
Gustavo <gustavo.margutti@rojemac.com.br>
2019-11-26 16:11:15 -0300

    Jobs - PNG

--- src/data/jobs.js
+++ src/data/jobs.js
@@ -1,29 +1,29 @@
 export default [
   {
-      img: '/public/assets/1',
+      img: '/public/assets/1.png',
   },
   {
-      img: '/public/assets/2',
+      img: '/public/assets/2.png',
   },
   {
-      img: '/public/assets/3',
+      img: '/public/assets/3.png',
   },
   {
-      img: '/public/assets/4',
+      img: '/public/assets/4.png',
   },
   {
-      img: '/public/assets/5',
+      img: '/public/assets/5.png',
   },
   {
-      img: '/public/assets/6',
+      img: '/public/assets/6.png',
   },
   {
-      img: '/public/assets/7',
+      img: '/public/assets/7.png',
   },
   {
-      img: '/public/assets/8',
+      img: '/public/assets/8.png',
   },
   {
-      img: '/public/assets/9',
+      img: '/public/assets/9.png',
   },
 ]
