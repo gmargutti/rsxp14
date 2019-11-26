18bb3ee56831e23e76785def51e2a39ef5cdc6b8
Gustavo <gustavo.margutti@rojemac.com.br>
2019-11-26 12:09:02 -0300

    Add Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+const RSXP = require('./components')
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <body>
+        <RSXP />
+      </body>
+    </div>
+  );
+}
+
+export default App;
