9a9b53fe73e2adbdaa3037afafa6472f6e083a84
Gustavo <gustavo.margutti@rojemac.com.br>
2019-11-26 17:51:18 -0300

    Remove test img

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -9,7 +9,6 @@ export default function Main() {
   return (
     <Fragment>
       <Header>
-        <img src={img} />
         <h1>Por que ser um progamador?</h1>
         <Chatbot />
       </Header>
