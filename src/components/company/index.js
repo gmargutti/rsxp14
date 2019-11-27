aee70d4eec475120affd032dc4bce89ea1b8b20c
Leandro Dias <leandro.dias_profissional@outlook.com.br>
2019-11-26 22:55:17 -0300

    inputs campos dinamicos

--- src/components/company/index.js
+++ src/components/company/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
+
 import { FaInstagram } from 'react-icons/fa';
 
 import { Card, Container } from './styles';
 
-export default function Cases() {
+export default function Cases({ description, company, link }) {
   return (
     <Container>
       <Card>
         <div className="logo">
           <FaInstagram fontSize={40} color="#259" />
         </div>
-        <title>Instagram</title>
         <section>
-          Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore
-          te sed. Elitr scripta ocurreret qui ad.
+          <b>{company}</b> {description}
         </section>
         <button>
-          <a href="/" target="_black">
+          <a href={link} target="_black">
             Conheça Mais
           </a>
         </button>
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -6,8 +6,10 @@ import Job from '../../components/job';
 import Case from '../../components/cases';
 
 import jobObject from '../../data/jobs';
+import companyObject from '../../data/companys';
 
 import { Header, Jobs, Companys, Cases } from './styles';
+import companys from '../../data/companys';
 
 export default function Main() {
   return (
@@ -16,14 +18,14 @@ export default function Main() {
       <Header>
         <h1>Por que ser um progamador?</h1>
         <h2>
-          Oque é ser programador ? Bem pela definição Em ciências da computação;
-          programador, desenvolvedor, codificador ou engenheiro de software é
-          alguém que escreve, desenvolve ou faz manutenção de software em um
-          grande sistema ou alguém que desenvolve software para uso em
-          computadores pessoais. Mas na verdade o programador é a pessoa que vai
-          mudar o mundo, “sim nós podemos” e devemos ser aqueles que mudam o dia
-          o dia da sociedade olha o impacto que teve o aplicativo do uber nubank
-          air b&b, whatsApp
+          <b>O que é ser programador ?</b> Bem pela definição Em ciências da
+          computação; programador, desenvolvedor, codificador ou engenheiro de
+          software é alguém que escreve, desenvolve ou faz manutenção de
+          software em um grande sistema ou alguém que desenvolve software para
+          uso em computadores pessoais. Mas na verdade o programador é a pessoa
+          que vai mudar o mundo, “sim nós podemos” e devemos ser aqueles que
+          mudam o dia o dia da sociedade olha o impacto que teve o aplicativo do
+          uber nubank air b&b, whatsApp
         </h2>
       </Header>
       <Jobs>
@@ -38,16 +40,15 @@ export default function Main() {
         ))}
       </Jobs>
       <Companys>
-        <div className="">
-          <Company />
-          <Company />
-          <Company />
-        </div>
-        <div className="">
-          <Company />
-          <Company />
-          <Company />
-        </div>
+        {companyObject.map(company => (
+          <div className="">
+            <Company
+              company={company.name}
+              description={company.description}
+              link={company.link}
+            />
+          </div>
+        ))}
       </Companys>
       <Cases>
         <div className="">
